2e44a5500f07963904d684ee7c45f2392cfa4668
waready <elbarto119@hotmail.com>
2019-08-16 17:58:58 -0500

    display

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,56 @@
 <template>
   <div>
     <b-container class="bv-example-row">
-      <b-row >
-        
-        <b-col sm="4" class="pl">
-           <b-button variant="primary" block>Codigo</b-button>
-        </b-col>
-        <b-col sm="4" class="pl">
-          <b-button variant="warning" block>Dni</b-button>
-        </b-col>
-        <b-col sm="4" class="pl">
-          <b-button variant="danger" block>Nombres</b-button>
-        </b-col>
-       
-      </b-row>
+      
+  <b-card
+    title="BUSCADOR"
+  >
+    <b-row >
+      <b-col sm="4" class="pl">
+        <b-button variant="primary" block @click="buscadorCodigo">Codigo</b-button>
+      </b-col>
+      <b-col sm="4" class="pl">
+        <b-button variant="warning" block @click="buscadorDni">Dni</b-button>
+      </b-col>
+      <b-col sm="4" class="pl">
+        <b-button variant="danger" block @click="buscadorNombres">Nombres</b-button>
+      </b-col>
+    </b-row>
+     
+    <em slot="footer">
+      <template v-if="buscador.codigo">
+        <b-input-group prepend="Busqueda" class="mt-3">
+          <b-form-input placeholder="ingrese busqueda (Codigo)"></b-form-input>
+          <b-input-group-append>
+            <b-button variant="success">Buscar</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </template>
+
+      <template v-if="buscador.dni">
+        <b-input-group prepend="Busqueda" class="mt-3">
+          <b-form-input placeholder="ingrese busqueda (Dni)"></b-form-input>
+          <b-input-group-append>
+            <b-button variant="success">Buscar</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </template>
+      
+      <template v-if="buscador.nombres">
+        <b-input-group prepend="Busqueda" class="mt-3">
+          <b-form-input placeholder="ingrese busqueda (Nombres)"></b-form-input>
+          <b-input-group-append>
+            <b-button variant="success">Buscar</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </template>
+
+    </em>
+  </b-card>
     </b-container>
+     <pre>
+      {{$data}}
+    </pre>
   </div> 
 </template>
 
@@ -23,7 +59,28 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      buscador:{
+        codigo:true,
+        dni:false,
+        nombres:false
+      }
+    }
+  },
+  methods:{
+    buscadorCodigo(){
+      this.buscador.codigo = true
+      this.buscador.dni = false
+      this.buscador.nombres = false
+    },
+    buscadorDni(){
+      this.buscador.dni = true
+      this.buscador.codigo = false
+      this.buscador.nombres = false
+    },
+    buscadorNombres(){
+      this.buscador.nombres = true
+      this.buscador.dni = false
+      this.buscador.codigo = false
     }
   }
 }
